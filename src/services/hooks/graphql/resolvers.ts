30542556cd2a1ead1bdda7916a9f7019d8e9b946
Douglas Maliszewski <douglas@lighthouseit.com.br>
2023-07-13 16:01:27 -0300

    envir

--- src/services/hooks/graphql/resolvers.ts
+++ src/services/hooks/graphql/resolvers.ts
@@ -4,7 +4,7 @@ const environment =
   process.env.NODE_ENV === "development"
     ? process.env.NEXT_PUBLIC_API_GRAPHQL_DEV
     : process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_GRAPHQL_PROD
+    ? "https://mills.vercel.app"
     : "https://mills.vercel.app";
 
   console.log(environment)
@@ -13,7 +13,7 @@ const resolvers = {
   Query: {
     imagesHome: async (parent: any, args: ParamsSearch) => {
       const result = await fetch(
-        `/api/images?description=${args.description}`
+        `${environment}/api/images?description=${args.description}`
       ).then((res) => res.json());
       return result;
     },
