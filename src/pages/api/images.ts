efe73bc4a69d97b71ac7f76a4de6f54bb0846987
Douglas Maliszewski <douglas@lighthouseit.com.br>
2023-07-13 16:53:55 -0300

    images

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -3,46 +3,49 @@ import ImagesHome from "@/dtos/ImagesHome";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<Array<ImagesHome>>
 ) {
   const { description } = req.query;
   const credentials = btoa(`${process.env.USER}:${process.env.SECRET}`);
   const auth = { Authorization: `Basic ${credentials}` };
   let channelToken = "";
   let resultImages: ImagesHome[] = [];
-  const data = await fetch(
-    `${process.env.ORACLE_CMS}/content/management/api/v1.1/channels`,
+  const obj = await fetch(
+    `${process.env.ORACLE_CMS}/content/published/api/v1.1/items/CONT9CB563D111134915AF82B76997FDA455?channelToken=9dc59eee7bb24f2795312a783c7b1743`,
     { headers: auth }
-  ).then((channelResponse) => channelResponse.json())
-  
-  resultImages[0].name = data[0].id
-  res.status(200).json(data)
-    // .then((channelData) => {
-    //   const channel = channelData.items.find(
-    //     (x: any) => x.createdBy === "cms-site@mills.com.br"
-    //   );
-    //   return fetch(
-    //     `${process.env.ORACLE_CMS}/content/management/api/v1.1/channels/${channel.id}`,
-    //     { headers: auth }
-    //   )
-    //     .then((channelDataResponse) => channelDataResponse.json())
-    //     .then((channelTokenData) => {
-    //       channelToken = channelTokenData.channelTokens[0].token;
-    //       return fetch(
-    //         `${process.env.ORACLE_CMS}/content/published/api/v1.1/items?q=description+co+"${description}"&channelToken=${channelToken}&fields=all`
-    //       )
-    //         .then((channelTokenResponse) => channelTokenResponse.json())
-    //         .catch((err) => {
-    //           console.error("Erro ao conectar" + err.message);
-    //         });
-    //     })
-    //     .catch((err) => {
-    //       console.error("Erro ao conectar" + err.message);
-    //     });
-    // })
-    // .catch((err) => {
-    //   console.error("Erro ao conectar" + err.message);
-    // });
+  ).then((res) => res.json());
+  resultImages.push(obj);
+  // const data = await fetch(
+  //   `${process.env.ORACLE_CMS}/content/management/api/v1.1/channels`,
+  //   { headers: auth }
+  // )
+  //   .then((channelResponse) => channelResponse.json())
+  //   .then((channelData) => {
+  //     const channel = channelData.items.find(
+  //       (x: any) => x.createdBy === "cms-site@mills.com.br"
+  //     );
+  //     return fetch(
+  //       `${process.env.ORACLE_CMS}/content/management/api/v1.1/channels/${channel.id}`,
+  //       { headers: auth }
+  //     )
+  //       .then((channelDataResponse) => channelDataResponse.json())
+  //       .then((channelTokenData) => {
+  //         channelToken = channelTokenData.channelTokens[0].token;
+  //         return fetch(
+  //           `${process.env.ORACLE_CMS}/content/published/api/v1.1/items?q=description+co+"${description}"&channelToken=${channelToken}&fields=all`
+  //         )
+  //           .then((channelTokenResponse) => channelTokenResponse.json())
+  //           .catch((err) => {
+  //             console.error("Erro ao conectar" + err.message);
+  //           });
+  //       })
+  //       .catch((err) => {
+  //         console.error("Erro ao conectar" + err.message);
+  //       });
+  //   })
+  //   .catch((err) => {
+  //     console.error("Erro ao conectar" + err.message);
+  //   });
 
   // for (let i = 0; i < data?.items.length; i++) {
   //   const obj = await fetch(
@@ -52,5 +55,5 @@ export default async function handler(
   //   resultImages.push(obj);
   // }
 
-  // res.status(200).json(resultImages);
+  res.status(200).json(resultImages);
 }
