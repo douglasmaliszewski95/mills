9c717b41207917517c40646bf7d17e348dbb28af
Douglas Maliszewski <douglas@lighthouseit.com.br>
2023-09-19 16:36:28 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -2,9 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    URL_API: process.env.NEXT_PUBLIC_API_GRAPHQL_QA,
+    URL_API: process.env.NEXT_PUBLIC_API_GRAPHQL_DEV,
     OCC_URL_STORE: process.env.OCC_URL_STORE,
     OCC_BEARER_TOKEN: process.env.OCC_BEARER_TOKEN,
+    MILLS_USER: process.env.MILLS_USER,
+    MILLS_SECRET: process.env.MILLS_SECRET,
+  },
+  images: {
+    domains: ["64.media.tumblr.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "contentinstance-fernandothome.cec.ocp.oraclecloud.com",
+      },
+    ],
   },
   async rewrites() {
     return [
@@ -13,16 +24,36 @@ const nextConfig = {
         destination: "/industrialMaintenance",
       },
       {
-        source: "/pesadas",
+        source: "/maquinas-pesadas",
         destination: "/heavyMachine",
       },
       {
-        source: "/buscar-equipamento",
-        destination: "/search",
+        source: "/maquinas-pesadas/busca",
+        destination: "/heavyMachine/search",
+      },
+      {
+        source: "/maquinas-pesadas/escavadeira",
+        destination: "/heavyMachine/excavator",
+      },
+      {
+        source: "/maquinas-pesadas/retroescavadeira",
+        destination: "/heavyMachine/backhoe",
+      },
+      {
+        source: "/maquinas-pesadas/motoniveladora",
+        destination: "/heavyMachine/motorGrader",
+      },
+      {
+        source: "/maquinas-pesadas/minicarregadeira",
+        destination: "/heavyMachine/smallLoader",
       },
       {
         source: "/plataformas-elevatorias",
         destination: "/liftingPlatform",
+      },
+      {
+        source: "/plataformas-elevatorias/busca",
+        destination: "/liftingPlatform/search",
       },
       {
         source: "/plataformas-elevatorias/pantografica-ou-tesoura",
@@ -37,17 +68,312 @@ const nextConfig = {
         destination: "/liftingPlatform/telescopic",
       },
       {
-        source: "/plataformas-elevatorias/:id",
-        destination: "/segments",
+        source: "/plataformas-elevatorias/montagem-de-estrutura-para-eventos",
+        destination: "/liftingPlatform/assemblyStructure",
+      },
+      {
+        source: "/plataformas-elevatorias/outros",
+        destination: "/liftingPlatform/others",
+      },
+      {
+        source: "/plataformas-elevatorias/instalacao-e-manutencao-industrial",
+        destination: "/liftingPlatform/industrialInstallationAndMaintenance",
+      },
+      {
+        source: "/plataformas-elevatorias/construcao-e-edificacao",
+        destination: "/liftingPlatform/constructionAndEdification",
+      },
+      {
+        source: "/plataformas-elevatorias/pintura-e-limpeza-em-altura",
+        destination: "/liftingPlatform/paintingAndCleaning",
+      },
+      {
+        source: "/plataformas-elevatorias/instalacao-e-manutencao-predial",
+        destination: "/liftingPlatform/installationAndMaintenance",
+      },
+      {
+        source: "/plataformas-elevatorias/pintura-e-limpeza-em-altura",
+        destination: "/liftingPlatform/paintingAndCleaning",
+      },
+      {
+        source: "/plataformas-elevatorias/inventario-e-organizacao-do-estoque",
+        destination: "/liftingPlatform/inventoryAndStockOrganization",
       },
       {
         source: "/produto",
         destination: "/product",
       },
+      {
+        source: "/geradores",
+        destination: "/generators",
+      },
+      {
+        source: "/geradores/industrias",
+        destination: "/generators/industries",
+      },
+      {
+        source: "/geradores/servicos-e-infraestrutura",
+        destination: "/generators/servicesAndInfrastructure",
+      },
+      {
+        source: "/geradores/construcao",
+        destination: "/generators/construction",
+      },
+      {
+        source: "/formas-e-escoramentos",
+        destination: "/moldsAndShoring",
+      },
+      {
+        source: "/compressores/industrias",
+        destination: "/generators/industries",
+      },
+      {
+        source: "/compressores/servicos-e-infraestrutura",
+        destination: "/generators/servicesAndInfrastructure",
+      },
+      {
+        source: "/compressores/construcao",
+        destination: "/generators/construction",
+      },
+      {
+        source: "/compressores",
+        destination: "/compressors",
+      },
+      {
+        source: "/formas-e-escoramentos",
+        destination: "/formworkAndShoring",
+      },
+      {
+        source: "/carrinho/passo-01",
+        destination: "/cart/step-01",
+      },
+      {
+        source: "/carrinho/passo-02",
+        destination: "/cart/step-02",
+      },
+      {
+        source: "/carrinho/passo-03",
+        destination: "/cart/step-03",
+      },
+      {
+        source: "/carrinho/orcamento-finalizado",
+        destination: "/cart/finalizedBudget",
+      },
+      {
+        source: "/formas-e-escoramentos/infraestrutura",
+        destination: "/moldsAndShoring/infrastructure",
+      },
+      {
+        source: "/formas-e-escoramentos/grandes-edificacoes",
+        destination: "/moldsAndShoring/bigBuildings",
+      },
+      {
+        source: "/formas-e-escoramentos/energia",
+        destination: "/moldsAndShoring/energy",
+      },
+      {
+        source: "/downloads",
+        destination: "/institutionalPages/downloads",
+      },
+      {
+        source: "/sobre-a-mills",
+        destination: "/institutionalPages/aboutMills",
+      },
+      {
+        source: "/mapa-de-atuacao",
+        destination: "/mapa-de-atuacao",
+      },
+      {
+        source: "/jornada-de-sustentabilidade",
+        destination: "/institutionalPages/sustainabilityJourney",
+      },
+      {
+        source: "/trabalhe-conosco",
+        destination: "/institutionalPages/workWithUs",
+      },
+      {
+        source: "/mills-na-midia",
+        destination: "/institutionalPages/millsMidia",
+      },
+      {
+        source: "/programa-de-integridade",
+        destination: "/institutionalPages/integrityProgram",
+      },
+      {
+        source: "/cases",
+        destination: "/institutionalPages/cases",
+      },
+      {
+        source: "/formas-e-escoramentos/industria",
+        destination: "/moldsAndShoring/industries",
+      },
+      {
+        source: "/formas-e-escoramentos/saneamento",
+        destination: "/moldsAndShoring/sanitation",
+      },
+      {
+        source: "/formas-e-escoramentos/transportes-e-mobilidade",
+        destination: "/moldsAndShoring/transport",
+      },
+      {
+        source: "/aluguel-de-plataformas-elevatorias",
+        destination: "/platformRental",
+      },
+      {
+        source: "/vendas-equipamentos",
+        destination: "/equipmentSelling",
+      },
+      {
+        source: "/frete",
+        destination: "/shipping",
+      },
+      {
+        source: "/manutencao-e-assistencia-tecnica",
+        destination: "/maintenance",
+      },
+      {
+        source: "/treinamento",
+        destination: "/training",
+      },
+      {
+        source: "/pecas",
+        destination: "/parts",
+      },
+      {
+        source: "/pecas/carrinho/passo-01",
+        destination: "/parts/cart/step-01",
+      },
+      {
+        source: "/pecas/carrinho/passo-02",
+        destination: "/parts/cart/step-02",
+      },
+      {
+        source: "/pecas/busca",
+        destination: "/parts/search",
+      },
+      {
+        source: "/pecas/carrinho/orcamento-finalizado",
+        destination: "/parts/cart/finalizedBudget",
+      },
+      {
+        source: "/vendas-seminovos-novos/carrinho/passo-01",
+        destination: "/equipmentSelling/cart/step-01",
+      },
+      {
+        source: "/vendas-seminovos-novos/carrinho/passo-02",
+        destination: "/equipmentSelling/cart/step-02",
+      },
+      {
+        source: "/vendas-seminovos-novos/busca",
+        destination: "/equipmentSelling/search",
+      },
+      {
+        source: "/vendas-seminovos-novos/carrinho/orcamento-finalizado",
+        destination: "/equipmentSelling/cart/finalizedBudget",
+      },
+      {
+        source: "/formas-e-escoramentos/carrinho/passo-01",
+        destination: "/moldsAndShoring/cart/step-01",
+      },
+      {
+        source: "/formas-e-escoramentos/carrinho/passo-02",
+        destination: "/moldsAndShoring/cart/step-02",
+      },
+      {
+        source: "/formas-e-escoramentos/busca",
+        destination: "/moldsAndShoring/search",
+      },
+      {
+        source: "/formas-e-escoramentos/carrinho/orcamento-finalizado",
+        destination: "/moldsAndShoring/cart/finalizedBudget",
+      },
+      {
+        source: "/duvidas-frequentes",
+        destination: "/faq",
+      },
+      {
+        source: "/politica-de-privacidade",
+        destination: "/privacyPolicy",
+      },
+      {
+        source: "/fale-conosco",
+        destination: "/contact",
+      },
+      {
+        source: "/cases",
+        destination: "/cases",
+      },
+      {
+        source: "/cases/:id",
+        destination: "/cases/singularCase/:id",
+        source: "/maquinas-pesadas/agronegocio",
+        destination: "/heavyMachine/agribusiness",
+      },
+      {
+        source: "/maquinas-pesadas/construcao",
+        destination: "/heavyMachine/construction",
+      },
+      {
+        source: "/maquinas-pesadas/florestal",
+        destination: "/heavyMachine/forestry",
+      },
+      {
+        source: "/maquinas-pesadas/mineracao",
+        destination: "/heavyMachine/mining",
+      },
+      {
+        source: "/maquinas-pesadas/portuario",
+        destination: "/heavyMachine/port",
+      },
+      {
+        source: "/maquinas-pesadas/outros",
+        destination: "/heavyMachine/others",
+      },
+      {
+        source: "/maquinas-pesadas/pa-carregadeira",
+        destination: "/heavyMachine/shovelLoader",
+      },
+      {
+        source: "/maquinas-pesadas/rolo-compactador",
+        destination: "/heavyMachine/roadRoller",
+      },
+      {
+        source: "/maquinas-pesadas/locacao",
+        destination: "/heavyMachine/rent",
+      },
+      {
+        source: "/maquinas-pesadas/manutencao",
+        destination: "/heavyMachine/maintenance",
+      },
+      {
+        source: "/maquinas-pesadas/acessorios-implementos",
+        destination: "/heavyMachine/accessoriesAndImplements",
+      },
+      {
+        source: "/maquinas-pesadas/telemetria-para-pesados",
+        destination: "/heavyMachine/telemetry",
+      },
+      {
+        source: "/maquinas-pesadas/frete",
+        destination: "/heavyMachine/freight",
+      },
+      {
+        source: "/maquinas-pesadas/venda-pecas",
+        destination: "/heavyMachine/partsSale",
+      },
+      {
+        source: "/maquinas-pesadas/cases",
+        destination: "/heavyMachine/cases",
+      },
+      {
+        source: "/maquinas-pesadas/trator-de-esteiras",
+        destination: "/heavyMachine/bulldozer",
+      },
+      {
+        source: "/mapa-de-atuacao",
+        destination: "/mapa-de-atuacao",
+      },
     ];
-  },
-  images: {
-    domains: ["contentinstance-fernandothome.cec.ocp.oraclecloud.com"],
   },
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ export const theme = {
       800: "#C75302",
     },
     green: {
+      10: "#0ED222",
       300: "#25D366",
       800: "#004042",
     },
@@ -37,23 +38,30 @@ export const theme = {
     },
     white: "#ffffff",
     beige: {
+      50: "#F9F7EE",
       100: "#EBEBEB",
       200: "#EBE3C7",
+      500: "#A0A0A0",
     },
     red: {
       600: "#FF585F",
+      800: "#B00020",
     },
     black: "#000000",
     gray: {
       25: "rgba(0, 0, 0, 0.60)",
+      30: "#CCCCCC",
       50: "#F8F8F8",
       75: "#F4F3F3",
+      80: "#F5F5F5",
       100: "#F6F6F6C7",
+      150: "#F6F6F6C7",
       200: "#E9E9E9",
       300: "#444444",
       400: "#CBCBCB",
       500: "#B6ADB479",
       700: "#424242",
+      800: "#D9D9D9",
     },
   },
   extend: {
@@ -62,6 +70,17 @@ export const theme = {
     },
     height: {
       478: "30rem",
+    },
+  },
+
+  extends: {
+    backgroundSize: {
+      mobile: "244px",
+    },
+    backgroundPosition: {
+      "top-right": "top right -175px;",
+      "top-right-mobile": "top right -15px;",
+      "bottom-right-mobile": "bottom right -15px;",
     },
   },
 };
